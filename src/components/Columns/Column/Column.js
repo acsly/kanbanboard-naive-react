b567d9f31a64d834f727e93495b9a740ebc730a7
RobitPC <ahmetcan@robit.com.tr>
2018-05-17 21:08:11 +0300

    card numbers are shown in column headers

--- src/components/Columns/Column/Column.js
+++ src/components/Columns/Column/Column.js
@@ -6,7 +6,7 @@ import Cards from '../../Cards/Cards';
 const column = (props) => (
     <div className={classes.Column}>
         <header>
-            <h4>{props.name}</h4>
+            <h4>{props.name} ({props.cards.length})</h4>
             <button onClick={props.newCardClicked}>new</button>
         </header>
         <div>
