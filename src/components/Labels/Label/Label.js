07e3cd6327517cbc30b21303d55095b71d871783
RobitPC <ahmetcan@robit.com.tr>
2018-05-20 03:57:47 +0300

    label update

--- src/components/Labels/Label/Label.js
+++ src/components/Labels/Label/Label.js
@@ -2,8 +2,8 @@ import React from 'react';
 import classes from './Label.css';
 
 const label = (props) => (
-    <p className={classes.Label} style={{ backgroundColor: props.color }}>{props.name} |
-        <button onClick={(cardIndex, columnIndex) => props.removeLabelClicked(props.id, cardIndex, columnIndex)}>X</button>
+    <p className={classes.Label} style={{ backgroundColor: props.color }}>{props.name}
+        <button className={classes.removeBtn} onClick={(cardIndex, columnIndex) => props.removeLabelClicked(props.id, cardIndex, columnIndex)}>X</button>
     </p>
 );
 
